384d6990a84825b5870b5928819f4fc7d6ee2d6c
Ken Payne <ken@meltano.com>
2023-01-24 17:26:59 +0000

    make suite_config a dataclass

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_stackexchange.tap import TapStackExchange
 
@@ -13,7 +13,7 @@ SAMPLE_CONFIG = {
     "metrics_log_level": "debug",
 }
 
-TEST_SUITE_CONFIG = {"ignore_no_records_for_streams": ["tag_synonyms"]}
+TEST_SUITE_CONFIG = SuiteConfig(ignore_no_records_for_streams=["tag_synonyms"])
 
 TestTapStackExchange = get_tap_test_class(
     tap_class=TapStackExchange, config=SAMPLE_CONFIG, suite_config=TEST_SUITE_CONFIG
